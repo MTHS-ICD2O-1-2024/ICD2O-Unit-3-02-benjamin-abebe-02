b271b4b75b814b93e833785f2bb50705858f4a5f
benjamin-abebe <benjamin.abebe@stu.ocsb.ca>
2025-03-31 17:29:41 +0000

    innit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Benjamin
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
+
+
+function myButtonClicked() {
+  // input
+  const height = document.getElementById("height-input").value
+  const lenght = document.getElementById("width-input").value
+  const width = document.getElementById("length-input").value
+  // area and perimeter
+  const area = ((height * lenght * width) / 3);
+
+  // displaying the answer of the area and perimeter
+  document.getElementById("answer").innerHTML = "The Volume is: " + area + "mm²";
+}
